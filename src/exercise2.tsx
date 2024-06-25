059ad302af766bb71ca88e82ff3081fd4fb6851c
Chris Lane Jones <chrislanejones@gmail.com>
2024-06-24 23:55:20 -0400

    TypeScript Study - Generic Classes - Type Inference

--- src/exercise2.tsx
+++ src/exercise2.tsx
@@ -21,3 +21,20 @@ const strArr = ["a", "b"];
 const stringFiltered = filter(strArr, (a) => a === "a");
 
 export default null;
+
+// Generic Classes
+class Foo<T1, T2> {
+  property1?: T1;
+  property2?: T2;
+
+  constructor(p1: T1, p2: T2) {
+    this.property1 = p1;
+    this.property2 = p2;
+  }
+}
+
+// TypeScript can look at the function and function
+// arguments to figure out "T" by working backwards
+let fooInst1 = new Foo<number, string>(12, "Hello");
+
+let fooInst2 = new Foo(12, "Hello");
