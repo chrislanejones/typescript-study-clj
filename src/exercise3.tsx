543cd4470a1447b48a13d9e58beb8fd6a545b203
Chris Lane Jones <chrislanejones@gmail.com>
2024-06-28 01:39:04 -0400

    TypeScript Study - Unions - Cat or Dog - someCat will not work

--- src/exercise3.tsx
+++ src/exercise3.tsx
@@ -2,30 +2,50 @@
 type Cat = {
   name: string;
   whiskers: number;
-  catBread: string;
+  catBreed: string;
+
+  TYPE: "Cat"
 };
+
+let myCat = {
+  Name: "Muffins",
+  whiskers: 12,
+  catBreed: "don't care",
+  TYPE: "Cat"
+}
+
+let someCat: Cat;
+
+someCat = myCat
 
 type Dog {
   name: string;
-  deadly: true
+  obedient: boolean;
+
+  TYPE: "Dog"
 };
 
-type lion {
-  name: string;
-  obedient: boolean;
-};
+// type Lion {
+//   name: string;
+//   deadly: true
+// };
 
 
 let catOrDog: Cat | Dog;
 let catAndDog: Cat & Dog;
 
-// If In property is actually Javascript
-function dealWithCatOrDog(animal: Cat | Dog | Lion) {
+// If In property is actually Javascript - Bad Version - Wont catch issues if more varibles involved
+function dealWithCatOrDog1(animal: Cat | Dog ) {
   if ("whiskers" in animal) {
     animal.catBread;
   } else {
     animal.obedient;
   }
 }
+
+
+// If In property is actually Javascript - Good Version
+function dealWithCatOrDog2(animal: Cat | Dog ) {
+  if (animal.TYPE === "Cat")
 
 export default null;
