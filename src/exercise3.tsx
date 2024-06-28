5c1eb35f2dc76837d2cb869f8a4069ef62918133
Chris Lane Jones <chrislanejones@gmail.com>
2024-06-27 23:59:49 -0400

    TypeScript Study - Unions - Cat or Dog - Broken Type

--- src/exercise3.tsx
+++ src/exercise3.tsx
@@ -7,14 +7,20 @@ type Cat = {
 
 type Dog {
   name: string;
+  deadly: true
+};
+
+type lion {
+  name: string;
   obedient: boolean;
 };
+
 
 let catOrDog: Cat | Dog;
 let catAndDog: Cat & Dog;
 
 // If In property is actually Javascript
-function dealWithCatOrDog(animal: Cat | Dog) {
+function dealWithCatOrDog(animal: Cat | Dog | Lion) {
   if ("whiskers" in animal) {
     animal.catBread;
   } else {
