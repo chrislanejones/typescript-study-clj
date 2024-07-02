1aa9c943eb45cb9aa012ee89024d096d980dd285
Chris Lane Jones <chrislanejones@gmail.com>
2024-07-01 21:53:31 -0400

    TypeScript Study - Advanced Stuff

---
+++ src/exercise5.tsx
@@ -0,0 +1,31 @@
+// Variance
+
+// The type asked for X but Y was submitted
+
+// Types of Variance
+// Covariance: providing something more specific than what you asked for
+// You asked for a shape, and I gave you a circle
+
+// Contravariance: providing
+// You asked for a circle, and I gave you a shape
+
+// Type Shape = {
+//   name: string = "";
+// }
+
+// Type Circle = Shape & {
+//   radius: number = 0;
+// }
+
+class Shape {
+  name: string = ""; 
+}
+
+class Circle extends Shape {
+  radius: number = 0;
+}
+
+// new Circle()
+
+let circle Circle = new Circle();
+let shape Circle = new Shape();
