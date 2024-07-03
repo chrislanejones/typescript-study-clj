c4664f9932114b8691f411669bbe69c2d5a501b7
Chris Lane Jones <chrislanejones@gmail.com>
2024-07-03 01:09:47 -0400

    TypeScript Study -  Conditional Types - Return Type of a Function

--- src/exercise6.tsx
+++ src/exercise6.tsx
@@ -1,10 +1,10 @@
 // Conditional types
 
-type Useless = Array<number> extends object ? true : never;
+type Useless = Array<number> extends string ? { val: true } : string;
 
-type IsArray<T> = T extends Array<unknown> ? true : false;
+type IsArray<T> = T extends Array<unknown> ? true : never;
 
-type A = IsArray<string[]>;
+type A = IsArray<number>;
 type B = IsArray<string>;
 
 // Inferring types
@@ -14,3 +14,12 @@ type ArrayOfWhat1<T> = T extends Array<infer U> ? U : never;
 type ArrayOfWhat2<T extends Array<unknown>> = T extends Array<infer U>
   ? U
   : never;
+
+// Type Utilites - Return Type of a Function
+type GetReturnType<T> = T extends (...args: any[]) => infer U ? U : never;
+
+const fn = ({} = 12);
+
+type X = GetReturnType<typeof for>;
+
+export default null;
