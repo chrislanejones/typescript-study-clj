1e1d60cca28df5ac54c639dbae4c9621b034cc8d
Chris Lane Jones <chrislanejones@gmail.com>
2024-07-04 22:51:35 -0400

    TypeScript Study - Object Properties With Keyof

--- src/exercise7.tsx
+++ src/exercise7.tsx
@@ -19,3 +19,44 @@ type Union = "A" | "B" | 12 | 42 | "C";
 type OnlyStrings<T> = T extends string ? T : never;
 
 type StringsInUnion = OnlyStrings<Union>;
+
+// Object properties with Keyof
+type Account = {
+  id: number;
+  name: string;
+  accountId: string;
+  gamerId: string;
+  myIdentification: string;
+};
+
+type IdFields = {
+  [K in keyof Account as K extends `${string}${"id" | "Id"}`
+    ? K
+    : never]: Account[K];
+};
+
+type IdFields2 = {
+  [K in keyof Account as K extends `${string}${"id" | "Id"}${string}`
+    ? K
+    : never]: Account[K];
+};
+
+type IdTypes = {
+  [K in keyof Account as K extends `${infer U}${"id" | "Id"}`
+    ? U
+    : never]: Account[K];
+};
+
+type IdTypes2 = {
+  [K in keyof Account as K extends `${infer U}${"id" | "Id"}`
+    ? U extends ""
+      ? never
+      : U
+    : never]: Account[K];
+};
+
+type IdTypes3 = {
+  [K in keyof Account as K extends `${infer U}${"Id"}` ? U : never]: Account[K];
+};
+
+export default null;
